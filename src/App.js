aff89a0e34a8f47c5a9e0a44f0f049d0ece8cdb8
Danny-Tran <truong_tran@live.com>
2020-08-27 14:56:07 -0600

    working on App Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Todos Apps</div>
-      </header>
+class App extends Component {
+  state = {
+    tasks: ['task 1','task 2','task 3']
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>Todos Apps</div>
+        </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
