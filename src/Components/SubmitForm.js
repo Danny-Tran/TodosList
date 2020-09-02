35a7a60c8eaeead5371b0fea871a46cb47fb3851
Danny-Tran <truong_tran@live.com>
2020-09-02 12:11:35 -0600

    add FormSubmit

---
+++ src/Components/SubmitForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SubmitForm extends Component {
+    render() {
+        return (
+            <div>
+                I'm Form submit
+            </div>
+        );
+    }
+}
+
+export default SubmitForm;
